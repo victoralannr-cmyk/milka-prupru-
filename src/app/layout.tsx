e8ed2fa2c873a22abdd6f29e1b31ccd9d0df48c5
Victor Alann <victorAlannr@gmail.com>
2025-11-16 03:32:45 +0000

    eu não quero que nada do meu site seja na fonte serif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
         {children}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['"Raleway"', 'sans-serif'],
-        headline: ['"Playfair Display"', 'serif'],
+        headline: ['"Raleway"', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {

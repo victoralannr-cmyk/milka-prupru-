3f3daa4db16d68c95f998dd738171c0fefc05704
Victor Alann <victorAlannr@gmail.com>
2025-11-16 03:40:12 +0000

    deixe os textos de todo o site em negrito

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased font-bold">
         {children}
         <Toaster />
       </body>

3680fe55cc9caa52eb88ed4eb55ea0e21f6e02aa
Victor Alann <victorAlannr@gmail.com>
2025-11-17 01:39:01 +0000

    na ultima aba coloque o fundo da ultima aba branco

--- src/components/sections/contact.tsx
+++ src/components/sections/contact.tsx
@@ -94,7 +94,7 @@ Gostaria de ser contatado(a)!`;
                   <FormItem>
                     <FormLabel>Nome Completo</FormLabel>
                     <FormControl>
-                      <Input placeholder="Seu nome" {...field} className="bg-white/80 text-black" />
+                      <Input placeholder="Seu nome" {...field} className="bg-white/80" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -107,7 +107,7 @@ Gostaria de ser contatado(a)!`;
                   <FormItem>
                     <FormLabel>Celular</FormLabel>
                     <FormControl>
-                      <Input placeholder="(XX) XXXXX-XXXX" {...field} onChange={(e) => handlePhoneInputChange(e, field)} className="bg-white/80 text-black" />
+                      <Input placeholder="(XX) XXXXX-XXXX" {...field} onChange={(e) => handlePhoneInputChange(e, field)} className="bg-white/80" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -120,7 +120,7 @@ Gostaria de ser contatado(a)!`;
                   <FormItem>
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input type="email" placeholder="seu@email.com" {...field} className="bg-white/80 text-black" />
+                      <Input type="email" placeholder="seu@email.com" {...field} className="bg-white/80" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -133,7 +133,7 @@ Gostaria de ser contatado(a)!`;
                   <FormItem>
                     <FormLabel>Mensagem Adicional (Opcional)</FormLabel>
                     <FormControl>
-                      <Textarea placeholder="Conte-nos um pouco sobre o que você precisa..." {...field} className="bg-white/80 text-black" />
+                      <Textarea placeholder="Conte-nos um pouco sobre o que você precisa..." {...field} className="bg-white/80" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
